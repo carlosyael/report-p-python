901d6bf1ed270dbef14b0fbfb1a0371931919376
carlosyael <carlosyael78645321@gmail.com>
2024-06-12 10:39:33 -0400

    actualizacion-python3.12.3

--- Proyecto Reportes/.venv/Scripts/activate.bat
+++ Proyecto Reportes/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\carlo\OneDrive\Documentos\Escritorio\Desarrollo\Python Oracle Connection Project\.venv
+set VIRTUAL_ENV=c:\Users\carlo\OneDrive\Documentos\Escritorio\Desarrollo\report-p-python repository\Proyecto Reportes\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
